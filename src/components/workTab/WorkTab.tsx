c8e9a00a188f860cff1b7f604fe4aade279dae4c
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-02-04 08:08:14 +0300

    features/#4/work_tab_is_done

--- src/components/workTab/WorkTab.tsx
+++ src/components/workTab/WorkTab.tsx
@@ -1,11 +1,42 @@
-import React from "react";
 import scss from "./workTab.module.scss";
+import work1 from "../../assets/worsTad.png";
+import wark2 from "../../assets/worw2.svg";
+import wark3 from "../../assets/oio.svg";
 
 const WorkTab = () => {
   return (
     <div className={scss.workTab}>
-      <div className="container">
-        <div className={scss.workTab}>workTab</div>
+      <div className={scss.container}>
+        <div className={scss.work}>
+          <h1>Как решать задачи на WorkTap?</h1>
+          <p>Идеально подходит для бизнеса и частных лиц</p>
+          <div className={scss.box}>
+            <div>
+              <img src={work1} alt="img" />
+              <p>Выберите услугу</p>
+              <p>
+                В супермаркете WorkTap представлен широкий <br /> выбор услуг от
+                квалифицированных специалистов.
+              </p>
+            </div>
+            <div>
+              <img src={wark2} alt="img" />
+              <p>Оплатите</p>
+              <p>
+                Деньги будут перечислены продавцу после <br /> того, как он
+                выполнит работу, и вы её одобрите.
+              </p>
+            </div>
+            <div>
+              <img src={wark3} alt="img" />
+              <p>Получите результат</p>
+              <p>
+                В супермаркете WorkTap представлен широкий <br /> выбор услуг от
+                квалифицированных специалистов.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
