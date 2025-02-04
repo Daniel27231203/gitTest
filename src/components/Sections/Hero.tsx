51fb21a33ca4dd3288ab6d4fe161271c3f7ec23d
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-02-04 08:58:20 +0300

    features/#2/fix

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -1,35 +1,22 @@
-import React from "react";
 import image from "../../assets/Group 11.png";
+import s from "./Hero.module.scss";
 const Hero = () => {
   return (
-    <section id="hero">
-      <div className="container">
-        <div className="hero">
-          <div className="ee">
-            <h1>
-              Покупайте фриланс-услуги в <span>два клика</span>
-            </h1>
-            <p>
-              Ворк — единица работы продавца, которую можно купить как товар в
-              магазине
-            </p>
-            <div className="box">
-              <input type="Что нужно сделать?" />
-              <button>Найти</button>
-            </div>
-            <p>Выберите рубрику, чтобы начать</p>
+    <section className={s.hero}>
+      <div className={s.container}>
+        <div className={s.hero}>
+          <div className={s.herOne}>
             <div>
-              <button>Тексты и переводы </button>
-              <button>Разработка</button>
-              <button>Дизайн</button>
-              <button>Аудио, видео монтаж </button>
-              <button>SEO и оптимизация</button>
-              <button>Бизнес и жизнь</button>
-              <button>Соцсети и реклама</button>
-              <button>Все категории</button>
+              <h1>Покупайте фриланс-услуги в два клика</h1>
+              <p>
+                Ворк — единица работы продавца, которую
+                <br /> можно купить как товар в магазине{" "}
+              </p>
+              <input type="text" placeholder="Что нужно сделать?" />
+              <p>Выберите рубрику, чтобы начать</p>
             </div>
+            <img src={image} alt="img" />
           </div>
-          <img src={image} alt="" />
         </div>
       </div>
     </section>
